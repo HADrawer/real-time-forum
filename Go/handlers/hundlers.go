d8765775e7d0e0f1eef67c6fe8a77246eca4fdf3
hashem <hashemalsaie0457@example.com>
2025-01-23 21:50:15 +0300

    added a first handlers

--- Go/handlers/hundlers.go
+++ Go/handlers/hundlers.go
@@ -1,5 +1,31 @@
 package handlers
 
 import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
+var templates = template.Must(template.ParseGlob("Views/*.html"))
+
+func RenderTemplate(w http.ResponseWriter) {
+	tmpl := filepath.Join("Views", "index.html")
+	if _, err := os.Stat(tmpl); os.IsNotExist(err) {
+		if _, err404 := os.Stat(filepath.Join(tmpl)); os.IsNotExist(err404) {
+			// 404.html is missing, directly return 404 error
+			return
+		}
+
+	}
+	
+	err := templates.ExecuteTemplate(w, "index.html",nil)
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w)
+}
