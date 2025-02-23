ffb7fe758fe6c920e2461afed18e9ad15aaeb52e
hashem <hashemalsaie0457@gmail.com>
2025-02-23 23:54:00 +0000

    adding websocket and intial chat

--- Go/handlers/websocket.go
+++ Go/handlers/websocket.go
@@ -1,45 +1,93 @@
 package handlers
 
 import (
+	database "Real-Time/Go/DB"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
+var clients = make(map[*websocket.Conn]string)
+var broadcast = make(chan Message)
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+type User struct {
+	ID        int    `json:"id"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"email"`
+}
+
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		// Allow all connections by default 
 		return true
 	},
 }
 
-// WebSocketHandler handles WebSocket connections
-func WebSocketHandler(w http.ResponseWriter, r *http.Request) {
-	// Upgrade the HTTP connection to a WebSocket connection
+func HandleConnections(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("WebSocket upgrade failed:", err)
+		log.Println(err)
 		return
 	}
-	defer conn.Close()
+	defer func() {
+		log.Println("Closing WebSocket connection")
+		conn.Close()
+	}()
 
-	log.Println("WebSocket connection established")
-
-	// Handle WebSocket messages
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("WebSocket read error:", err)
-			break
-		}
-
-		log.Printf("Received WebSocket message: %s", message)
-
-		// Echo the message back to the client
-		if err := conn.WriteMessage(messageType, message); err != nil {
-			log.Println("WebSocket write error:", err)
-			break
-		}
+	var msg Message
+	err = conn.ReadJSON(&msg)
+	if err != nil {
+		log.Println("Error reading username:", err)	
+			return
 	}
+
+	clients[conn] = msg.Username
+	fmt.Println(msg.Username, "connected")
+
+	for {
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+            log.Println("Error reading message:", err)		
+			delete(clients, conn)
+			break
+		}
+
+		broadcast <- msg
+	}
+}
+
+func HandleMessages() {
+	for {
+		msg := <-broadcast
+
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+
+	}
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	users, err := database.GetAllUsers()
+	if err != nil {
+		http.Error(w, "Unable to fetch users", http.StatusInternalServerError)
+		return
+	}
+
+	// Send the list of users as a JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(users)
 }
--- main.go
+++ main.go
@@ -1,48 +1,45 @@
 package main
 
 import (
-	"Real-Time/Go/DB"
+	database "Real-Time/Go/DB"
 	"Real-Time/Go/handlers"
 	"log"
 	"net/http"
-	
+
 )
 
-func main(){
+func main() {
 	database.Init()
 
-
-
-	http.HandleFunc("/", handlers.HomeHandler )
-	http.HandleFunc("/login", handlers.LoginHandler )
-	http.HandleFunc("/register", handlers.RegisterHandler )
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
-		handlers.DestroySession(w,r)
-		http.Redirect(w,r,"/login",http.StatusSeeOther)
+		handlers.DestroySession(w, r)
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
 	})
 	// http.HandleFunc("/Direct", handlers.MessagesHandler)
 	http.HandleFunc("/Create", handlers.CreatePostHandler)
 	http.HandleFunc("/Post", handlers.ViewPostHandler)
-    http.HandleFunc("/Comment", handlers.CommentHandler)
-
-
-
+	http.HandleFunc("/Comment", handlers.CommentHandler)
 
 	http.HandleFunc("/api/create-data", handlers.CreateDataHandler)
 	http.HandleFunc("/api/home-data", handlers.HomeDataHandler)
 	http.HandleFunc("/api/post-data", handlers.PostDataHandler)
 	http.HandleFunc("/Direct", handlers.DirectHandler)
+	http.HandleFunc("/ws", handlers.HandleConnections)
+	http.HandleFunc("/users", handlers.GetUsers)
+
+	go handlers.HandleMessages()
 
 
 	log.Print("Server is running on http://localhost:8080")
 
-
-	http.Handle("/Css/", http.StripPrefix("/Css/",http.FileServer(http.Dir("Css"))))
-	http.Handle("/Js/", http.StripPrefix("/Js/",http.FileServer(http.Dir("Js"))))
+	http.Handle("/Css/", http.StripPrefix("/Css/", http.FileServer(http.Dir("Css"))))
+	http.Handle("/Js/", http.StripPrefix("/Js/", http.FileServer(http.Dir("Js"))))
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Failed to start server: ", err)
 	}
-
 
 }
